2d0e4daaef402d8b156128e6b6698dea541166f6
sohaib <sohaibmaqsoodsohaibmaqsood@gmail.com>
2025-07-01 15:01:05 +0500

    update

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+
+const Navbar= ()=>{
+    return(
+        <>
+        <div className="Navbar">
+            <div className="logo">
+
+            </div>
+            <div className="Nav_options">
+                <ul>
+                    <li><a href="">Home</a></li>    
+                    <li><a href="">About</a></li>    
+                    <li><a href="">Contact</a></li>        
+                </ul>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Navbar
